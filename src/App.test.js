00df77edc5ce0985e3c4ee8a34e9571101f17081
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-04-09 00:10:05 -0400

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('checks for title', () => {
+  const div = document.createElement('div');
+  render(<App/>, div);
+  expect(div.innerHTML).toMatch('Hi there!')
+  ReactDOM.unmountComponentAtNode(div);
+})
